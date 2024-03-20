55b502acc5630733c51055769c9431ce6d1911e8
Simon Bukin <8992420+simonbukin@users.noreply.github.com>
2024-03-19 22:02:31 -0700

    Disable Tailwind preflight

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,9 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  corePlugins: {
+    preflight: false,
+  },
   theme: {
     extend: {
       backgroundImage: {
