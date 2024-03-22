2216226c6a539e1e7ea808bc718e66f2637aa42d
Simon Bukin <8992420+simonbukin@users.noreply.github.com>
2024-03-22 14:38:37 -0700

    Revert to simpler loader

--- src/components/MTGCard.tsx
+++ src/components/MTGCard.tsx
@@ -2,7 +2,6 @@ import { BasicCard } from "@/types/cards";
 import Image from "next/image";
 import { motion } from "framer-motion";
 import { useState } from "react";
-import { Skeleton } from "@mantine/core";
 
 interface IMTGCardProps {
   card: BasicCard;
@@ -15,23 +14,19 @@ function MTGCard({ card }: IMTGCardProps) {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.25 }}
-      className="relative"
+      className="relative flex flex-col items-center justify-center"
     >
-      <Skeleton
-        className="max-w-full h-auto border top-0 left-0 absolute"
-        height="100%"
-        width="100%"
-        visible={!loaded}
-      >
-        <Image
-          onLoad={() => setLoaded(true)}
-          className="rounded-lg"
-          src={card.imageUrl}
-          alt={`The Magic card ${card.name}`}
-          width={667}
-          height={930}
-        />
-      </Skeleton>
+      <Image
+        onLoad={() => setLoaded(true)}
+        className="rounded-lg"
+        style={{
+          border: !loaded ? "1px solid gainsboro" : "",
+        }}
+        src={card.imageUrl}
+        alt={`The Magic card ${card.name}`}
+        width={667}
+        height={930}
+      />
     </motion.div>
   );
 }
